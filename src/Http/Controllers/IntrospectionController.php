4e2ec9e18f54ffdfe09a9401a624a4fe9aed0dfe
pm <pm@101media.ru>
2020-09-28 10:13:32 +0300

    Meet Laravel 8

--- src/Http/Controllers/IntrospectionController.php
+++ src/Http/Controllers/IntrospectionController.php
@@ -3,6 +3,8 @@
 namespace Ipunkt\Laravel\OAuthIntrospection\Http\Controllers;
 
 use Illuminate\Http\JsonResponse;
+use Illuminate\Support\Arr;
+use Illuminate\Support\Str;
 use Laravel\Passport\Bridge\AccessTokenRepository;
 use Laravel\Passport\ClientRepository;
 use Laravel\Passport\Passport;
@@ -70,12 +72,12 @@ class IntrospectionController
 		try {
 			$this->resourceServer->validateAuthenticatedRequest($request);
 
-			if (array_get($request->getParsedBody(), 'token_type_hint', 'access_token') !== 'access_token') {
+			if (Arr::get($request->getParsedBody(), 'token_type_hint', 'access_token') !== 'access_token') {
 				//  unsupported introspection
 				return $this->notActiveResponse();
 			}
 
-			$accessToken = array_get($request->getParsedBody(), 'token');
+			$accessToken = Arr::get($request->getParsedBody(), 'token');
 			if ($accessToken === null) {
 				return $this->notActiveResponse();
 			}
@@ -190,7 +192,7 @@ class IntrospectionController
 	{
 		return $this->errorResponse([
 			'error' => [
-				'id' => str_slug(get_class($exception) . ' ' . $status),
+				'id' => Str::slug(get_class($exception) . ' ' . $status),
 				'status' => $status,
 				'title' => $exception->getMessage(),
 				'detail' => $exception->getTraceAsString()
